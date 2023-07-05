b1ab28eac602e80a1a0d6247647a33f7311451c1
leonardof02 <leonardoarencibia2002@gmail.com>
2023-07-05 13:03:38 -0400

    Change the app name in title

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -4,7 +4,7 @@ import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import route from '../../vendor/tightenco/ziggy/dist/index.m';
 
-const appName = 'Laravel';
+const appName = 'QR Menu';
 
 createServer((page) =>
     createInertiaApp({
